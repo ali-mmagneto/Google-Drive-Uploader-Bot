ffe474be99df8e3e1c256c8a5293f14f12aa72c1
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2022-05-24 00:09:08 +0300

    Update TEXT.py

--- plugins/TEXT.py
+++ plugins/TEXT.py
@@ -1,7 +1,7 @@
 
 
 # Here is Your Drive Folder Name You can Replace it with Your Desire name(Optional)
-drive_folder_name = "DcGDriveUploaderBot"
+drive_folder_name = "AnagrupBotArsiv"
 
 
 # Enter Your Mega email And Pass (Required)
